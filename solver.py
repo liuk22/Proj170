4e722ff7530c22098a3d92fe9e0ecc935f942f88
oqin <qin.olivia00@Gmail.com>
2020-04-21 15:07:06 -0700

    fixed bug

--- solver.py
+++ solver.py
@@ -29,10 +29,10 @@ def solve(G):
 
 
 def can_remove_leaf(leaf, MST):
-    for leaf_neighbor in MST.neighbors(leaf):
+    for leaf_neighbor in G.neighbors(leaf):
         # leaf_neighbor must have at least one neighbor that's in MST that is not leaf
         current_leaf_neighbor_OK = False
-        for leaf_neighbor_neighbor in MST.neighbors(leaf_neighbor):
+        for leaf_neighbor_neighbor in G.neighbors(leaf_neighbor):
             if MST.has_node(leaf_neighbor_neighbor) and leaf_neighbor_neighbor != leaf:
                 current_leaf_neighbor_OK = True
         if not current_leaf_neighbor_OK:
@@ -48,6 +48,7 @@ if __name__ == '__main__':
     for file in os.listdir('./inputs'):
         G = read_input_file('./inputs/' + file, 100)
         T = solve(G)
+#        assert False
         assert is_valid_network(G, T)
         print("Average  pairwise distance: {}".format(average_pairwise_distance(T)))
-        write_output_file(T, 'out/{0}.out'.format(file[:-3]))
+        write_output_file(T, 'outputs/{0}.out'.format(file[:-3]))
